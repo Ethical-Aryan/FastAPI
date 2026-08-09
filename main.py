48db97c6a2da0612b34fba7615beaa9112e329a6
Ethical-Aryan <bughunteraryan12@gmail.com>
2026-08-09 13:13:53 +0530

    Creating JSON

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
+from fastapi.responses import JSONResponse
 
 obj = FastAPI()
 
 @obj.get("/display")
 def display():
     return {"Welcome to first API"}
+
+@obj.get("/home")
+def home():
+    return JSONResponse(content={"data":"Welcome to JSON"})
