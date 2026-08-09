5346d41be2449688d567666a610e41deed9bc22c
Ethical-Aryan <bughunteraryan12@gmail.com>
2026-08-09 13:41:35 +0530

    Single and Multiple Parameter

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from fastapi.responses import JSONResponse
 
 obj = FastAPI()
 
-@obj.get("/display")
-def display():
-    return {"Welcome to first API"}
+#Single Query parameter
+@obj.get("/single")
+def single(name):
+    message = "Welcome " + name
+    return JSONResponse(status_code=200,content={"data":message })
+
+#Multi Query Parameter ,fixing datatype and giving default value
+@obj.get("/multiple") 
+def multiple(id:int,name:str,mobile:int,email:str,address=""):
+    try:
+        message = "Welcome " + str(id) + name + str(mobile) + email + address   
+        return JSONResponse(status_code=200,content={"data":message })
+    except Exception as e:
+         return JSONResponse(status_code=500,content={"data":str(e) })  
 
 @obj.get("/home")
 def home():
