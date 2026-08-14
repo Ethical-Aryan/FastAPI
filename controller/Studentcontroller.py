f751a4a720cb1dfaac749e39113f7c2d1c28c335
Ethical-Aryan <bughunteraryan12@gmail.com>
2026-08-14 20:10:45 +0530

    Added Database

--- controller/Studentcontroller.py
+++ controller/Studentcontroller.py
@@ -7,16 +7,13 @@ def insert(request: StudentRequest):
     try:
         # Build response payload
         response = StudentResponse(
-            id=request.id,
             name=request.name,
+            gender=request.gender,
             mobile=str(request.mobile),
             email=request.email, 
-            address=request.address,
-            enroll_id="ENROLL_101",
-            subjects=["Python", "FastAPI"]
         )
         return JSONResponse(status_code=201, content={"response": response.model_dump()})
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
 
-
+
--- model/Studentmodel.py
+++ model/Studentmodel.py
@@ -3,11 +3,10 @@ from typing import List, Optional
 
 # Connects: router/Studentrouter.py & controller/Studentcontroller.py
 class StudentRequest(BaseModel):
-    id: int
     name: str
+    gender: str
     mobile: str
     email: str
-    address: str
 
 # Connects: controller/Studentcontroller.py
 class StudentResponse(BaseModel):
